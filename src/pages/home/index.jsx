96a598ae52a58efb6c29c243f253e1d28e00c95e
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-02-02 14:39:54 +0300

    changes

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1 className="text-red-500 font-permaMarker text-center text-7xl">
+        Home
+      </h1>
+    </div>
+  );
+};
+
+export default Index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F99417",
+        secondary: "#FFF",
+        alternative: "#222831",
+      },
+      fontFamily: {
+        permaMarker: ["Permanent Marker", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
