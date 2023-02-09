5fe41641472b996e5ecbc8fbdd2e4eabcde497fe
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-02-09 21:31:36 +0300

    dropdown menu finished

--- src/components/ui/DropdownMenu.jsx
+++ src/components/ui/DropdownMenu.jsx
@@ -1,37 +1,40 @@
-import OutsideClickHandler from "react-outside-click-handler";
 import { AiFillHome } from "react-icons/ai";
 import { GiBookCover } from "react-icons/gi";
 import { BsInfoSquareFill } from "react-icons/bs";
 import { MdDinnerDining } from "react-icons/md";
 
-const DropdownMenu = ({ setIsDropdownMenu }) => {
+const DropdownMenu = ({ isDropdownMenu }) => {
   return (
-    <div className="grid place-content-center">
-      <nav className="relative w-screen h-screen text-lg p-5 bg-[#F9B420] grid items-center">
-        <ul className="grid items-center gap-4 uppercase text-alternative">
-          <li className="hover:text-secondary transition-all">
-            <a href="#" className="flex justify-center items-center gap-3">
+    <div
+      className={`relative right-[-100vw] grid place-content-center  ${
+        isDropdownMenu && "menu-landing"
+      } transition-all`}
+    >
+      <nav className="w-screen h-screen text-lg p-5 bg-[#F9B420] grid place-content-center">
+        <ul className="flex flex-col items-end gap-4 uppercase text-alternative">
+          <li className="hover:text-secondary transition-all w-36">
+            <a href="#" className="flex justify-between items-center">
               <AiFillHome size={24} />
               <span>home</span>
             </a>
           </li>
-          <div className="border-b border-alternative w-[75px]" />
-          <li className="hover:text-secondary transition-all">
-            <a href="#" className="flex justify-center items-center gap-3">
+          <div className="border-b border-alternative w-[54px]" />
+          <li className="hover:text-secondary transition-all w-36">
+            <a href="#" className="flex items-center justify-between">
               <GiBookCover size={24} />
               <span>menu</span>
             </a>
           </li>
-          <div className="border-b border-alternative w-[90px]" />
-          <li className="hover:text-secondary transition-all">
-            <a href="#" className="flex justify-center items-center gap-3">
+          <div className="border-b border-alternative w-[80px]" />
+          <li className="hover:text-secondary transition-all w-36">
+            <a href="#" className="flex justify-between items-center">
               <BsInfoSquareFill size={24} />
               <span>about</span>
             </a>
           </li>
-          <div className="border-b border-alternative w-[105px]" />
-          <li className="hover:text-secondary transition-all">
-            <a href="#" className="flex justify-center items-center gap-3">
+          <div className="border-b border-alternative w-[106px]" />
+          <li className="hover:text-secondary transition-all w-36">
+            <a href="#" className="flex justify-between items-center">
               <MdDinnerDining size={24} />
               <span>book table</span>
             </a>
