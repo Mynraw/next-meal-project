198ed767cba95a412129cae0b1e99a2db0c76dc9
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-02-13 15:26:42 +0300

    campaign section has finalized

--- src/components/Campaigns.jsx
+++ src/components/Campaigns.jsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
 import { GiShoppingBag } from "react-icons/gi";
 
-const Campaigns = () => {
+const style = {
+  objectCover: "fit",
+};
+
+const CampaignItem = () => {
   return (
     <div className="container my-20 grid grid-cols-2 gap-4 mx-auto">
       <div className="col-span-2 md:col-span-1 flex sm:flex-row md:flex-col lg:flex-row items-center gap-3 justify-start bg-alternative p-5 rounded-sm w-full group">
-        <div className="border-[3px] rounded-full border-primary p-0 m-0 w-[180px] h-[180px] relative">
+        <div className="border-[3px] rounded-full border-primary p-0 m-0 w-[180px] h-[180px] relative overflow-hidden">
           <Image
-            src="/images/f11.jpg"
+            src="/images/f10.jpg"
             alt="campaign item"
             fill
             className="rounded-full group-hover:scale-110 transition-all"
+            style={style}
           />
         </div>
         <div className="text-white grid gap-3">
-          <h2 className="font-permaMarker text-xl">Tasty Thursdays</h2>
+          <h2 className="font-permaMarker text-xl">memeful thursdays</h2>
           <p className="font-permaMarker">
             <span className="text-5xl">20%</span> off
           </p>
@@ -24,16 +29,17 @@ const Campaigns = () => {
         </div>
       </div>
       <div className="col-span-2 md:col-span-1 flex sm:flex-row md:flex-col lg:flex-row items-center gap-3 justify-start bg-alternative p-5 rounded-sm w-full group">
-        <div className="border-[3px] rounded-full border-primary p-0 m-0 w-[180px] h-[180px] relative">
+        <div className="border-[3px] z-50 rounded-full border-primary p-2 m-0 w-[180px] h-[180px] relative overflow-hidden">
           <Image
             src="/images/f11.jpg"
             alt="campaign item"
             fill
             className="rounded-full group-hover:scale-110 transition-all"
+            style={style}
           />
         </div>
         <div className="text-white grid gap-3">
-          <h2 className="font-permaMarker text-xl">Tasty Thursdays</h2>
+          <h2 className="font-permaMarker text-xl">memeful thursdays</h2>
           <p className="font-permaMarker">
             <span className="text-5xl">20%</span> off
           </p>
@@ -43,6 +49,14 @@ const Campaigns = () => {
         </div>
       </div>
     </div>
+  );
+};
+
+const Campaigns = () => {
+  return (
+    <>
+      <CampaignItem />
+    </>
   );
 };
 
