24326c301fa5408281197d9b42e8e3ff6bce00be
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-02-17 17:41:35 +0300

    profile dropdown init

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -47,6 +47,37 @@ const Header = () => {
             </ul>
           </nav>
           <div className="flex gap-5 items-center">
+            <div class="dropdown group">
+              <button>
+                <FaUserAlt />
+              </button>
+              <ul class="dropdown-menu absolute hidden text-black group-hover:block">
+                <li class="">
+                  <a
+                    class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                    href="#"
+                  >
+                    One
+                  </a>
+                </li>
+                <li class="">
+                  <a
+                    class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                    href="#"
+                  >
+                    Two
+                  </a>
+                </li>
+                <li class="">
+                  <a
+                    class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                    href="#"
+                  >
+                    Three is the magic number
+                  </a>
+                </li>
+              </ul>
+            </div>
             <Link href="/" className="hover:text-primary transition-all">
               <FaUserAlt />
             </Link>
@@ -63,7 +94,8 @@ const Header = () => {
               <BsSearch />
             </button>
             <Link
-              href="/"
+              href="https://getir.com/yemek/restoran/food-homemade-burger-eryaman-mah-etimesgut-ankara/"
+              target="_blank"
               className="btn-primary capitalize hidden sm:inline-block"
             >
               order online
