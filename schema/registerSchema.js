2831df79154e919109ca98c4a3e51a428580f015
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-02-28 10:17:51 +0300

    regex additions on register form validation

---
+++ schema/registerSchema.js
@@ -0,0 +1,25 @@
+import * as Yup from "yup";
+
+export const registerSchema = Yup.object({
+  fullName: Yup.string()
+    .required("Full name is required")
+    .min(3, "Full name must be at least characters long."),
+  eMail: Yup.string()
+    .email("Please enter an valid e-mail address.")
+    .matches(
+      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      "Should be an valid e-mail adress."
+    )
+    .required("E-mail address is required."),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters long")
+    .matches(/[0-9]/, "Password requires a number")
+    .matches(/[a-z]/, "Password requires a lowercase letter")
+    .matches(/[A-Z]/, "Password requires an uppercase letter")
+    .matches(/[^\w]/, "Password requires a symbol")
+    .required("Password can't be empty."),
+  passwordMatch: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords doesn't match."
+  ),
+});
