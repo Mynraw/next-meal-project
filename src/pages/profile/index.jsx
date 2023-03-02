057b6a99c575c65850ab728928109e0bde1cde02
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-03-02 10:50:21 +0300

    profile page updated

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -17,8 +17,8 @@ const Index = () => {
   return (
     <div className="py-16">
       <div className="px-10 grid grid-cols-5">
-        <div>
-          <div className="relative w-24 h-24">
+        <div className="flex flex-col gap-3 col-span-1 border border-gray-300 pt-5">
+          <div className="relative w-24 h-24 mx-auto">
             <Image
               src={"/images/kekw.jpg"}
               alt="goat"
@@ -27,25 +27,27 @@ const Index = () => {
               style={style}
             />
           </div>
-          <span>John Doe</span>
-        </div>
-        <div>
-          <button>
-            <AiFillHome />
-            <span>account</span>
-          </button>
-          <button>
-            <HiKey />
-            <span>password</span>
-          </button>
-          <button>
-            <RiEBike2Fill />
-            <span>orders</span>
-          </button>
-          <button>
-            <MdLogout />
-            <span>logout</span>
-          </button>
+          <div className="mx-auto">
+            <span className="text-2xl font-bold">John Doe</span>
+          </div>
+          <div className="text-black">
+            <button className="flex items-center gap-2 py-3 px-2 w-full border-t border-gray-300 hover:bg-primary hover:text-white transition-all">
+              <AiFillHome />
+              <span className="capitalize">account</span>
+            </button>
+            <button className="flex items-center gap-2 py-3 px-2 w-full border-t border-gray-300 hover:bg-primary hover:text-white transition-all">
+              <HiKey />
+              <span className="capitalize">password</span>
+            </button>
+            <button className="flex items-center gap-2 py-3 px-2 w-full border-t border-gray-300 hover:bg-primary hover:text-white transition-all">
+              <RiEBike2Fill />
+              <span className="capitalize">orders</span>
+            </button>
+            <button className="flex items-center gap-2 py-3 px-2 w-full border-t border-gray-300 hover:bg-primary hover:text-white transition-all">
+              <MdLogout />
+              <span className="capitalize">logout</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
