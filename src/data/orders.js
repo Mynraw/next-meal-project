a16077a02b9d72b237a432b84fd7ef4e5350ec01
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-03-04 21:13:23 +0300

    profile page has finalized

---
+++ src/data/orders.js
@@ -0,0 +1,37 @@
+export const orders = [
+  {
+    id: 67236512,
+    address: "Yorkshire KEKW",
+    date: "2022-09-01",
+    total: 18,
+    status: 4,
+  },
+  {
+    id: 67236863,
+    address: "Yorkshire KEKW",
+    date: "2022-09-11",
+    total: 20,
+    status: 0,
+  },
+  {
+    id: 67236922,
+    address: "Yorkshire KEKW",
+    date: "2022-11-21",
+    total: 23,
+    status: 4,
+  },
+  {
+    id: 67237016,
+    address: "Yorkshire KEKW",
+    date: "2022-12-13",
+    total: 18,
+    status: 4,
+  },
+  {
+    id: 67237279,
+    address: "Yorkshire KEKW",
+    date: "2022-12-25",
+    total: 52,
+    status: 4,
+  },
+];
--- src/pages/profile/Orders.jsx
+++ src/pages/profile/Orders.jsx
@@ -1,18 +1,35 @@
+import { orders } from "@/data/orders";
+
 const Orders = () => {
   return (
     <div className="my-10">
-      <table>
-        <thead>
-          <tr>
-            <td>id</td>
-            <td>address</td>
-            <td>date</td>
-            <td>total</td>
-            <td>status</td>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
+      <div className="overflow-x-auto max-h-72 shadow-xl">
+        <table className="bg-alternative text-gray-400 font-extralight w-full rounded-lg">
+          <thead className="bg-[#354257] rounded-lg">
+            <tr className="uppercase font-medium rounded-lg">
+              <td className="py-2 px-3 rounded-tl-lg">id</td>
+              <td className="py-2 px-3">address</td>
+              <td className="py-2 px-3">date</td>
+              <td className="py-2 px-3">total</td>
+              <td className="py-2 px-3 rounded-tr-lg">status</td>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr
+                key={order.id}
+                className="hover:bg-[#f9b420] hover:text-white transition-all"
+              >
+                <td className="p-3 whitespace-nowrap">{order.id}</td>
+                <td className="p-3 whitespace-nowrap">{order.address}</td>
+                <td className="p-3 whitespace-nowrap">{order.date}</td>
+                <td className="p-3 whitespace-nowrap">${order.total}</td>
+                <td className="p-3 whitespace-nowrap">{order.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
