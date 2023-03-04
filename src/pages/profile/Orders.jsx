6c9d3f4da09b550fc45b3c896672e4df9360e8ff
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-03-04 20:15:14 +0300

    account and password components has finalized

---
+++ src/pages/profile/Orders.jsx
@@ -0,0 +1,20 @@
+const Orders = () => {
+  return (
+    <div className="my-10">
+      <table>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>address</td>
+            <td>date</td>
+            <td>total</td>
+            <td>status</td>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Orders;
