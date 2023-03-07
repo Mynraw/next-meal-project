c3fd786d127d7db59cdb1deb913d67db952f49af
Arya A <aryaacikgoz@outlook.com>
2023-03-07 20:57:49 +0300

    product meal size, ingredient price differs added

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import Title from "@/components/ui/Title";
 
@@ -5,7 +6,44 @@ const style = {
   objectFit: "contain",
 };
 
+const extras = [
+  {
+    id: 1,
+    name: "Extra cheese",
+    price: 3,
+  },
+  {
+    id: 2,
+    name: "SJW Sauce",
+    price: 2,
+  },
+  {
+    id: 3,
+    name: "Double trouble",
+    price: 7,
+  },
+];
+
 const Index = () => {
+  const [prices, setPrices] = useState([10, 20, 30]);
+  const [totalPrice, setTotalPrice] = useState(prices[0]);
+  const [mealSize, setMealSize] = useState(0);
+  const [ingredients, setIngredients] = useState(extras);
+
+  const handleProductSize = (size) => {
+    const priceDiffer = prices[size] - prices[mealSize];
+    setMealSize(size);
+    setTotalPrice((prevTotal) => (prevTotal += priceDiffer));
+  };
+
+  const handleIngredients = (extra, item) => {
+    const isChecked = extra.target.checked;
+
+    isChecked
+      ? setTotalPrice((prevTotal) => (prevTotal += item.price))
+      : setTotalPrice((prevTotal) => (prevTotal -= item.price));
+  };
+
   return (
     <div className="grid place-items-center">
       <div className="container h-screen w-screen grid grid-cols-1 md:grid-cols-2 items-start md:items-center gap-x-28 mt-28 md:mt-0">
@@ -15,7 +53,7 @@ const Index = () => {
         <div className="relative flex flex-col gap-y-6 items-center md:items-start">
           <Title addClass="text-4xl">item</Title>
           <span className="text-primary underline underline-offset-4 text-xl">
-            $ price
+            $ {totalPrice}
           </span>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
@@ -25,19 +63,19 @@ const Index = () => {
           <div>
             <strong className="block text-2xl mb-1">Choose the size</strong>
             <div className="flex items-center gap-10">
-              <button className="relative">
+              <button className="relative" onClick={() => handleProductSize(0)}>
                 <Image src="/images/size.png" alt="" width={60} height={60} />
                 <span className="bg-primary text-black text-sm rounded-lg px-3 absolute left-5 bottom-12 overflow-hidden">
                   Small
                 </span>
               </button>
-              <button className="relative">
+              <button className="relative" onClick={() => handleProductSize(1)}>
                 <Image src="/images/size.png" alt="" width={80} height={80} />
                 <span className="bg-primary text-black text-sm rounded-lg px-3 absolute left-7 bottom-16 overflow-hidden">
                   Medium
                 </span>
               </button>
-              <button className="relative">
+              <button className="relative" onClick={() => handleProductSize(2)}>
                 <Image src="/images/size.png" alt="" width={100} height={100} />
                 <span className="bg-primary text-black text-sm rounded-lg px-3 absolute left-10 bottom-20 overflow-hidden">
                   Large
@@ -45,33 +83,27 @@ const Index = () => {
               </button>
             </div>
           </div>
-          <div className="">
+          <div>
             <strong className="block text-2xl mb-2">
               Choose additional ingredients
             </strong>
-            <div className="flex items-center justify-center md:justify-start gap-x-1 mb-1">
-              <input
-                className="w-5 h-5 accent-primary"
-                id="test"
-                type={"checkbox"}
-              />
-              <label className="mr-3" htmlFor="test">
-                ketchup
-              </label>
-              <input
-                className="w-5 h-5 accent-primary"
-                id="test2"
-                type={"checkbox"}
-              />
-              <label className="mr-3" htmlFor="test2">
-                test
-              </label>
-              <input
-                className="w-5 h-5 accent-primary"
-                id="test3"
-                type={"checkbox"}
-              />
-              <label htmlFor={"test3"}>test</label>
+            <div className="flex">
+              {ingredients.map((ingredient) => (
+                <div
+                  key={ingredient.id}
+                  className="flex items-center justify-center md:justify-start gap-x-1 mb-1"
+                >
+                  <input
+                    className="w-5 h-5 accent-primary"
+                    id={ingredient.id}
+                    type={"checkbox"}
+                    onChange={(e) => handleIngredients(e, ingredient)}
+                  />
+                  <label className="mr-3" htmlFor={ingredient.id}>
+                    {ingredient.name}
+                  </label>
+                </div>
+              ))}
             </div>
           </div>
           <button className="btn-primary max-w-[150px]">Add to cart</button>
