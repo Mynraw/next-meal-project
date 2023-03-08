7d7f5ff74bca380b2f7bc58c40274e4319a3209c
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-03-08 17:16:58 +0300

    user creation model, get and post handler

--- src/models/User.js
+++ src/models/User.js
@@ -1,42 +1,41 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-        name: {
-            type: String,
-            required: true,
-        },
-        phoneNumber: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        address: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-        },
-        bio: {
-            type: String,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        passwordConfirm: {
-            type: String,
-            required: true,
-        },
-        emailVerified: {
-            type: Boolean,
-            default: false,
-        }
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-{timestamps: true},
+    phoneNumber: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    passwordConfirm: {
+      type: String,
+      required: true,
+    },
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,8 +1,30 @@
+import User from "@/models/User";
 import dbConnect from "@/lib/dbConnect";
-const handler = async(req, res) => {
-    await dbConnect();
-    res.status(200).json({message: "works?"});
 
-}
+const handler = async (req, res) => {
+  await dbConnect();
+
+  const { method } = req;
+
+  // find user
+  if (method === "GET") {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      console.log(`Error:  ${(error, error.status)}`);
+    }
+  }
+
+  // create user
+  if (method == "POST") {
+    try {
+      const newUser = await User.create(req.body);
+      res.status(200).json(newUser);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
 
 export default handler;
